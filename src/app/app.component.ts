97d1372c9898346acbc2088fb105605f83b03d21
steve16bit <heystevenn@gmail.com>
2025-03-28 03:08:54 -0300

    [004] - Responsivity and contact links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CardComponent } from "./components/card/card.component";
 import { CardMainComponent } from "./components/card-main/card-main.component";
 import { CardSuccessRouteComponent } from "./components/card-success-route/card-success-route.component";
 import { CardAboutComponent } from "./components/card-about/card-about.component";
@@ -8,7 +6,7 @@ import { CardContactComponent } from "./components/card-contact/card-contact.com
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent, CardMainComponent, CardSuccessRouteComponent, CardAboutComponent, CardContactComponent],
+  imports: [CardMainComponent, CardSuccessRouteComponent, CardAboutComponent, CardContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
